1be8c9b0b515c2d12c41255a0ad9a2c958e8491a
Amay Jain <93967321+jainamay19112002@users.noreply.github.com>
2026-08-31 15:29:18 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string ans;
+        for(auto s:strs){
+            ans.append(to_string(s.size()));
+            ans.push_back('#');
+            ans.append(s);
+        }
+        return ans;
+
+    }
+
+    vector<string> decode(string s) {
+        vector<string>ans;
+        int i=0;
+        while(i<s.size()){
+            int j=i;
+            while(s[j]!='#'){
+                j++;
+            }
+            int len=stoi(s.substr(i,j-i));
+            i=j+1;
+            j=i+len;
+            ans.push_back(s.substr(i,len));
+            i=j;
+        }
+        return ans;
+
+    }
+};
