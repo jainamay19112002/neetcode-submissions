56275c5334ac4e3e7f874ba881acf2d257f66a30
Amay Jain <93967321+jainamay19112002@users.noreply.github.com>
2026-09-02 09:15:20 +0530

    Add: reverse-integer - submission-0

---
+++ Data Structures & Algorithms/reverse-integer/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long ans=0;
+
+    while(x!=0) {
+        int lastdigit=x%10;
+        x=x/10;
+
+        ans=ans*10+lastdigit;
+    }
+
+    if(ans>INT_MAX||ans<INT_MIN)
+        return 0;
+
+    return ans;
+        
+    }
+};
