8ac2581d07a382bca4130149bcf375a76ee068ba
Amay Jain <93967321+jainamay19112002@users.noreply.github.com>
2026-09-02 08:40:04 +0530

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int xoring=0;
+        for(int i=0;i<=n;i++){
+            xoring=xoring^i;
+        }
+
+        for(int i=0;i<n;i++){
+            xoring=xoring^nums[i];
+        }
+        return xoring;
+        
+    }
+};
