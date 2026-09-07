9ae43aba57bdc897151e66c654df93107f2ebac8
Amay Jain <93967321+jainamay19112002@users.noreply.github.com>
2026-09-08 03:39:37 +0530

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool checkdfs(vector<vector<char>>& board, string word,int i,int j,int idx){
+        int n=board.size();
+        int m=board[0].size();
+        if(idx==word.size())
+        return true;
+
+        if(i<0 || j<0 || i>=n || j>=m || board[i][j]!=word[idx]){
+            return false;
+        }
+
+        char temp=board[i][j];
+        board[i][j]='#';
+
+        bool found=checkdfs(board,word,i-1,j,idx+1)|| checkdfs(board,word,i,j+1,idx+1)||
+                   checkdfs(board,word,i+1,j,idx+1)|| checkdfs(board,word,i,j-1,idx+1);
+
+                   board[i][j]=temp;
+
+                   return found;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int n=board.size();
+        int m=board[0].size();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(checkdfs(board,word,i,j,0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
