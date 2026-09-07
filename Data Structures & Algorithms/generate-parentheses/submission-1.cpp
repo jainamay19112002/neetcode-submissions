4c5da7b4ed80b6036f1bc86aa642b792bcdd2467
Amay Jain <93967321+jainamay19112002@users.noreply.github.com>
2026-09-08 03:06:07 +0530

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void backtrack(int openbracket,int closebracket,int n,vector<string>&ans,string s){
+        if(openbracket==closebracket && openbracket==n && closebracket==n){
+            ans.push_back(s);
+        }
+
+        if(openbracket<n){
+            s+='(';
+            backtrack(openbracket+1,closebracket,n,ans,s);
+            s.pop_back();
+        }
+
+        if(closebracket<openbracket){
+            s+=')';
+            backtrack(openbracket,closebracket+1,n,ans,s);
+            s.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string>ans;
+        string s;
+        backtrack(0,0,n,ans,s);
+        return ans;
+        
+    }
+};
