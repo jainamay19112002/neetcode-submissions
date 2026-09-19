54ff8fd4bc64590463763fffa88348f6fe4776da
Amay Jain <93967321+jainamay19112002@users.noreply.github.com>
2026-09-19 22:00:59 +0530

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) {
+        int n=board.size();
+        int m=board[0].size();
+
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        queue<pair<int,int>>q;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 || i==n-1 || j==0 || j==m-1){
+                    if(board[i][j]=='O'){
+                    q.push({i,j});
+                    vis[i][j]=1;
+                    }
+                }
+            }
+        }
+        int delrow[]={-1,0,+1,0};
+        int delcol[]={0,+1,0,-1};
+
+        while(!q.empty()){
+            int r=q.front().first;
+            int c=q.front().second;
+            q.pop();
+
+            for(int k=0;k<4;k++){
+                int nr=r+delrow[k];
+                int nc=c+delcol[k];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && !vis[nr][nc] && board[nr][nc]=='O'){
+                    q.push({nr,nc});
+                    vis[nr][nc]=1;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='O' && vis[i][j]==0){
+                    board[i][j]='X';
+                }
+            }
+        }
+        
+    }
+};
